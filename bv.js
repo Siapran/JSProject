16226d47b122f3c03ec594759d2215a6c78d34d7
Porte <porte.pierreantoine@gmail.com>
2015-03-18 19:49:52 +0100

    JS tabs dynamicly + save

--- bv.js
+++ bv.js
@@ -1,4 +1,5 @@
 $(function() {
+	initDocuments();
 	$("#accordion").accordion({
 		heightStyle: "fill"
 	});
@@ -16,24 +17,45 @@ function save(element) {
 			title = $(this).text();
 		else if ($(this).attr("class") == $(".content").attr("class"))
 			content = $(this).text();
+		else if ($(this).attr("class") == $(".id").attr("class"))
+			id = $(this).text();
 	});
 
 	var myDocument = new Object();
 	myDocument.myTitle = title;
 	myDocument.myContent = content + "}"; // sino nca bug...
-$.ajax({
-	url: "http://178.62.102.228/JSProjectSymfony/web/app.php/add/document/",
-	type: "POST", //send it through get method
-	dataType: "jsonp",
-	data: JSON.stringify({title:title, content:content}), // our data
-	success: function(response) { // success callback
-		console.log(response); // IF SUCCESS, IT'S SAVED AUTOMATICALLY IN SERVER
-	},
-	error: function(xhr) { // error callback
-		console.log("error");
-	}
-});
 
-	console.log(title);
-	console.log(content);
+	$.ajax({
+	  method: "POST",
+	  url: "http://178.62.102.228/JSProjectSymfony/web/app.php/add/document/",
+	  data: { myId:id, myTitle:title, myContent: content }
+	})
+	.done(function() {
+		alert("Document saved");
+	});
 }
+
+function initDocuments() {
+	$.ajax({
+		method: "GET",
+	 	url: "http://178.62.102.228/JSProjectSymfony/web/app.php/get/documents",
+	})
+	.done(function(data) {
+		$.each(data, function() {
+			console.log($(this)[0]);
+			var elem = $(this)[0];
+			var li = $("<li/>");
+			$("<a/>").attr("href", "#tab" + elem.id).text(elem.title).appendTo(li);
+			$("#listOfTabs").append(li);
+			$("#tabs")
+			// set all var of the document to add it after to the html page
+			var title = $("<h4/>").text("Title :").attr("class", "inline");
+			var id = $("<div/>").attr("class", "id").text(elem.id);
+			var titleEdit = $("<div/>").attr("class", "title").attr("contenteditable", "true").append($("<h1/>").text(elem.title))
+			var tools = $("<div/>").attr("class", "tools").html('<ul class="listOfTools"><li onclick="save(this)"><img src="images/disk.png"></li><li><img src="images/blue-document-page-next.png"></li><li><img src="images/blue-document-page-previous.png"></li><li><img src="images/edit-alignment-left.png"></li><li><img src="images/edit-alignment-center.png"></li><li><img src="images/edit-alignment-right.png"></li><li><img src="images/edit-italic.png"></li><li><img src="images/edit-bold.png"></li><li><img src="images/edit-underline.png"></li><li><img src="images/edit-list.png"></li><li><img src="images/edit-list-order.png"></li></ul>');
+			var content = $("<div/>").attr("class", "content").attr("contenteditable", "true").text(elem.content);
+			$("#tabs").append($("<div/>").attr("id", "tab" + elem.id).append(title).append(id).append(titleEdit).append(tools).append(content));
+		})
+	});
+}
+
