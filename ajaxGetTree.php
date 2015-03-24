224821cddf1f1b5f5534ce1d0a3ca24d7340e90f
Siapran <siapran.candoris@gmail.com>
2015-03-24 14:37:27 +0100

    tri de l'arborescence

--- ajaxGetTree.php
+++ ajaxGetTree.php
@@ -1,34 +1,47 @@
 <?php
 function get_tree($dir) {
-	$handle = opendir($dir);
 	$res = "";
-	$file = readdir($handle);
 
-	while (false !== $file) {
-		if ($file[0] != ".") {
-			$res .= "{";
-			$res .= "\"text\": \"".$file."\"";
-			$path = $dir."/".$file;
-			if (is_dir($path)) {
-				$res .= ", \"icon\": \"images/blue-folder.png\"";
-				$res .= ", \"children\": [".get_tree($path)."]";
-			} else {
-				$res .= ", \"icon\": \"images/blue-document.png\"";
-			}
-			$res .= "}";
-			if (($file = readdir($handle)) !== false)
-				$res .= ",";
-		} else $file = readdir($handle);
-	}
+	$handle = opendir($dir);
+	$files = array();
+	$dirs = array();
+
+	while (($file = readdir($handle)) !== false)
+		if ($file[0] != ".")
+			if (is_dir($file))
+				$dirs[] = $file;
+			else
+				$files[] = $file;
 
 	closedir($handle);
+
+	sort($files);
+	sort($dirs);
+	$files = array_merge($dirs, $files);
+
+	for ($i=0; $i < count($files); $i++) {
+		$file = $files[$i];
+		$res .= "{";
+		$res .= "\"text\": \"".$file."\"";
+		$path = $dir."/".$file;
+		if (is_dir($path)) {
+			$res .= ", \"icon\": \"images/blue-folder.png\"";
+			$res .= ", \"children\": [".get_tree($path)."]";
+		} else {
+			$res .= ", \"icon\": \"images/blue-document.png\"";
+		}
+		$res .= "}";
+		if ($i + 1 != count($files)) {
+			$res .= ",";
+		}
+	}
 
 	return $res;
 }
 
 echo "{ \"core\" : {
     \"data\" : [";
-echo get_tree($_GET["dir"]);
+echo get_tree("..");
 // echo get_tree($_GET["dir"]);
 echo "]
 } }";
