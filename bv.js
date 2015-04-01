cdbdda78247d98864a307cb285ea0847c7cdfa69
PORTE Pierre-Antoine <portepi@hostname.iut2.upmf-grenoble.fr>
2015-04-01 12:35:21 +0200

    Cannot open directory

--- bv.js
+++ bv.js
@@ -37,42 +37,6 @@ $(function() {
     $('#tree')
       // listen for event
       .on('changed.jstree', function (e, data) {
-      	console.log(data);
-      	if (data.action == "select_node") {
-			var path = data.instance.get_path(data.node,'/');
-			var alreadyThere = false;
-	      	$.ajax({
-	      	    async : true,
-	      	    type : "POST",
-	      	    url : "./ajaxJSProjectGet.php?",
-	      	    data: {"file":path},
-
-	      	    success : function(contenuFichier) {
-	      	    	$("div#tabs ul#listOfTabs li").each(function(index) {
-	      	    		if ($(this).children().text() == data.node.text) alreadyThere = true;
-	      	    	})
-	      	    	if (!alreadyThere) {
-		      	    	var tools = $("<div/>").attr("class", "tools").html('<ul class="listOfTools"><li onclick="saveDoc(this)"><img src="images/disk.png"></li><li><img src="images/blue-document-page-next.png"></li><li><img src="images/blue-document-page-previous.png"></li><li><img src="images/edit-alignment-left.png"></li><li><img src="images/edit-alignment-center.png"></li><li><img src="images/edit-alignment-right.png"></li><li><img src="images/edit-italic.png"></li><li><img src="images/edit-bold.png"></li><li><img src="images/edit-underline.png"></li><li><img src="images/edit-list.png"></li><li><img src="images/edit-list-order.png"></li></ul>');
-		      	    	var content = $("<div/>").attr("class", "content").attr("contenteditable", "true").text(contenuFichier);
-	    		        var num_tabs = $("div#tabs ul#listOfTabs li").length + 1;
-	    		        $("div#tabs ul#listOfTabs").append("<li><a href='#tab" + num_tabs + "'>" + data.node.text + "</a></li>");
-	    				$("div#tabs").append("<div id='tab" + num_tabs + "'><h4 class='inline'>Title : </h4><div class='title' id='titleEditable' contenteditable='true'>" + data.node.text + "</div>" + "</div>");
-	    		        $("div#tabs").tabs("refresh");
-	    		        $("div#tab" + num_tabs).append(tools);
-	    		        $("div#tab" + num_tabs).append(content);
-	    		        $("div#tab" + num_tabs).data("path", path);
-	    		        $("div#tabs" ).tabs( "option", "active", $("div#tabs ul#listOfTabs li").length-1);
-	      	    	}
-	      	    },    
-
-	      	    error : function(xhr, ajaxOptions, thrownError) {
-	      	        alert(xhr.status);
-	      	        alert(thrownError);
-	      	    }
-	      	});
-      	}
-		
-      	console.log("changed.jstree");
       });
       $('#tree').on('rename_node', function (e, data) {
       	console.log("rename_node");
@@ -80,10 +44,53 @@ $(function() {
       $('#tree').on('move_node', function (e, data) {
       	console.log("move_node");
       });
-      $('#tree').on('remove_node', function (e, data) {
-      	console.log("remove_node");
+      $('#tree').on('delete_node', function (e, data) {
+      	console.log("delete_node");
       	$('#tree').delete_node(data.node);
       });
+      $('#tree').on('select_node.jstree', function (e, data) {
+      	console.log("select_node");
+      	console.log(data);
+      	console.log(data.node.type);
+
+		var path = data.instance.get_path(data.node,'/');
+		var alreadyThere = false;
+      	$.ajax({
+      	    async : true,
+      	    type : "POST",
+      	    url : "./ajaxJSProjectGet.php?",
+      	    data: {"file":path},
+
+      	    success : function(contenuFichier) {
+      	    	$("div#tabs ul#listOfTabs li").each(function(index) {
+      	    		if ($(this).children().text() == data.node.text) {
+      	    			alreadyThere = true;
+    		        	$("div#tabs" ).tabs( "option", "active", index);
+      	    		} 
+      	    	})
+      	    	if (!alreadyThere && data.node.icon !== "images/blue-folder.png") {
+      	    		console.log($(this));
+	      	    	var tools = $("<div/>").attr("class", "tools").html('<ul class="listOfTools"><li onclick="saveDoc(this)"><img src="images/disk.png"></li><li><img src="images/blue-document-page-next.png"></li><li><img src="images/blue-document-page-previous.png"></li><li><img src="images/edit-alignment-left.png"></li><li><img src="images/edit-alignment-center.png"></li><li><img src="images/edit-alignment-right.png"></li><li><img src="images/edit-italic.png"></li><li><img src="images/edit-bold.png"></li><li><img src="images/edit-underline.png"></li><li><img src="images/edit-list.png"></li><li><img src="images/edit-list-order.png"></li></ul>');
+	      	    	var content = $("<div/>").attr("class", "content").attr("contenteditable", "true").text(contenuFichier);
+    		        var num_tabs = $("div#tabs ul#listOfTabs li").length + 1;
+    		        $("div#tabs ul#listOfTabs").append("<li><a href='#tab" + num_tabs + "'>" + data.node.text + "</a></li>");
+    				$("div#tabs").append("<div id='tab" + num_tabs + "'><h4 class='inline'>Title : </h4><div class='title' id='titleEditable' contenteditable='true'>" + data.node.text + "</div>" + "</div>");
+    		        $("div#tabs").tabs("refresh");
+    		        $("div#tab" + num_tabs).append(tools);
+    		        $("div#tab" + num_tabs).append(content);
+    		        $("div#tab" + num_tabs).data("path", path);
+    		        $("div#tabs" ).tabs( "option", "active", $("div#tabs ul#listOfTabs li").length-1);
+      	    	}
+      	    },    
+
+      	    error : function(xhr, ajaxOptions, thrownError) {
+      	        alert(xhr.status);
+      	        alert(thrownError);
+      	    }
+      	});
+      });
+            
+
 	//font
 	size = 1;
 
