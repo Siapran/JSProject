616fda5e58d1185d5b78e8f519fb142cb333c479
siapran <siapran.candoris@gmail.com>
2015-04-02 19:44:15 +0200

    fix JSProjectAdd.php

--- ajaxJSProjectAdd.php
+++ ajaxJSProjectAdd.php
@@ -1,12 +1,13 @@
 <?php
 
-	$path = "./homeDir/" . $_POST["file"];
+	$path = "homeDir/" . $_POST["file"]
 
 	if (!file_exists(dirname($path))) {
 		mkdir(dirname($path), 0777, true);
 	}
 	$handle = fopen($path, 'w') or die('Cannot open file:  '.$path);
-	fwrite($handle, $data);
+	fwrite($handle, $_POST["content"]);
 	fclose($handle);
+
 
 ?>
