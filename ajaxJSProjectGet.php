aa086d24fbd5f5e216710d102fa3c2ada2fcc521
siapran <siapran.candoris@gmail.com>
2015-04-02 19:53:43 +0200

    fix JSProjectGet.php

--- ajaxJSProjectGet.php
+++ ajaxJSProjectGet.php
@@ -1,7 +1,7 @@
 <?php
 $f = fopen("./homeDir/" . $_POST["file"] , "r");
 if (filesize("./homeDir/" . $_POST["file"]) > 0) {
-	$data = fread($f, );
+	$data = fread($f, filesize("./homeDir/" . $_POST["file"]));
 
 	echo $data;
 }
